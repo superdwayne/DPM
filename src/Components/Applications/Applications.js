2ea0ad2a21d783f46629a3c1da294c1973d16f78
Dwayne Paisley-Marshall <dwayne.p-marshall@farfetch.com>
2021-09-27 10:39:04 +0100

    Pushing final changes live

--- src/Components/Applications/Applications.js
+++ src/Components/Applications/Applications.js
@@ -20,7 +20,7 @@ function ani() {
                             <h3>Problem/Challange</h3>
                             <h4>We had a baby during the height of lockdown. We wanted to people to meet her safely so I decided to build a booking application so people could meet her, virtually.</h4>
                         </section>
-
+ 
                         <section className="project-info">
                                 <h3>Thinking Strategy & Approach</h3>
                                 <h4>I needed to set up something quickly so I decided to use Mongo (via mongoose) as the databse where the entries would be stored, the information was sent via a nodeJS end point </h4>
