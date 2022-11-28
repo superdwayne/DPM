a25f2cc8f42efed1a40d89bf0400dfcb5a644486
Dwayne Paisley-Marshall <superdwayne@gmail.com>
2022-11-28 16:11:32 +0100

    Remove API functioanlity

--- src/api/Context/Context_main.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const context_main = React.createContext({});
-
-export { context_main };
--- src/api/requests.js
+++
@@ -1,8 +0,0 @@
-const Request = (url, params, cb) => {
-    fetch(url, params)
-      .then((response) => response.json())
-      .then((response) => cb(response))
-      .catch((error) => alert(error));
-  };
-  
-  export default Request;
