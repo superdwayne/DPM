b90cbbaddf4cb90ffc814af6e7890b85570f6c92
Dwayne Paisley-Marshall <superdwayne@gmail.com>
2022-11-29 13:58:33 +0100

    Copy update

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function Loader(props) {
   <Html wrapperClass center> 
     <section className="intro">
       <header className="dpm">
-      <h1 style={{opacity: `${progress / 200}`, color: '#fff', fontSize: '150px', fontWeight: 'bolder'}}><span className='highlight'>DPM</span> IS {Math.ceil(progress)} % {active ? null : ''} A <span className='highlight'> LEAD CREATIVE</span> TECHNOLOGIST</h1>
+      <h1 style={{opacity: `${progress / 200}`, color: '#fff', fontSize: '150px', fontWeight: 'bolder'}}><span className='highlight'>DPM</span> IS {Math.ceil(progress)} % {active ? null : ''} A <span className='highlight'> CREATIVE</span> TECHNOLOGIST</h1>
       </header>
       
     </section>
