ec2a13949074bc319140abe1d2a8f31dac4800d4
Dwayne Paisley-Marshall <dwaynepaisley-marshall@dwaynes-mbp.kpn>
2024-01-26 12:12:03 +0100

    Change copy

--- src/App.js
+++ src/App.js
@@ -443,10 +443,10 @@ By deploying augmented reality, the garden of the summer venue became an interac
       </div>
       <footer>
         <section className="top" >
-          <h1>OPEN TO WORK</h1>
-          <h1>FROM JAN 2024</h1>
+          <h1>AVAILABLE FOR WORK</h1>
+          <h1>LET'S START A PROJECT</h1>
         </section>
-        <section className="middle"><h1 className="soria">LET'S START A PROJECT <br /> <a className="soria" href="mailto:hello@dwaynep-marshall.co.uk">hello@dwaynep-marshall.co.uk</a></h1>
+        <section className="middle"><h1 className="soria"> <a className="soria" href="mailto:hello@dwaynep-marshall.co.uk">hello@dwaynep-marshall.co.uk</a></h1>
 
           </section>
         <section className="bottom">
