70e3a1339f6ef76c880414f41f2ebd86c103f8e9
Dwayne Paisley-Marshall <superdwayne@gmai.com>
2025-05-15 15:39:39 +0200

    Vercel fix

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+yarn install --ignore-engines
+yarn build 
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,9 +8,15 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    emptyOutDir: true,
   },
   // Ensure SPA routing works properly
   server: {
     historyApiFallback: true,
-  }
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
 })
